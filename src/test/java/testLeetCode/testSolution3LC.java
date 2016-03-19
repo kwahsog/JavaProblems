b303df51c05bd908b1e0fa7b4a263d9f1ed86673
Alex <cooper.alexander@gmail.com>
2016-03-19 23:42:44 +0000

    clean code for 3LC testing up

--- src/test/java/testLeetCode/testSolution3LC.java
+++ src/test/java/testLeetCode/testSolution3LC.java
@@ -4,12 +4,20 @@ import leetcode.Solution3LC;
 
 public class testSolution3LC {
 
+	//testing Solution1LC. Return length of longest substring without repeating characters.
 	public static void main (String[] args){
 		
-		String a = "dvdf";
-		String b = "aaaaaaaaaab";
+		String str1 = "dvdf";
+		String str2 = "aaaaaaaaaab";
+		String str3 = "asdfadzadfazgp";
 		Solution3LC test = new Solution3LC();
-		System.out.println(test.lengthOfLongestSubstring(a));
-		System.out.println(test.lengthOfLongestSubstring(b));
+		
+		//expected 3
+		System.out.println(test.lengthOfLongestSubstring(str1));
+		//expected 2
+		System.out.println(test.lengthOfLongestSubstring(str2));
+		//expected 6
+		System.out.println(test.lengthOfLongestSubstring(str3));
+		
 	}
 }
